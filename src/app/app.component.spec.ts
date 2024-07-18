7ceecd0cd93bfb425be5432679dc3617f7065fb8
olatu <olya.latun@taxsystems.com>
2024-07-18 13:34:09 +0300

    chore: common app tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,44 @@
-import { TestBed } from '@angular/core/testing'
-import { RouterTestingModule } from '@angular/router/testing'
-import { AppComponent } from './app.component'
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { AuthService } from './modules/auth/services/auth-service/auth.service';
+import { AuthApiService } from './modules/auth/services/auth-api-service/auth.api.service';
+import { UserService } from './services/user.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
     beforeEach(() =>
         TestBed.configureTestingModule({
             imports: [RouterTestingModule],
             declarations: [AppComponent],
-        })
-    )
+            providers: [
+                AuthService,
+                AuthApiService,
+                HttpClient,
+                HttpHandler,
+                {
+                    provide: UserService,
+                    useValue: jasmine.createSpyObj('UserService', [
+                        'setCurrentUser',
+                        'getUserToken',
+                        'getUserID',
+                    ]),
+                },
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        }),
+    );
 
     it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        const app = fixture.componentInstance
-        expect(app).toBeTruthy()
-    })
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-    it(`should have as title 'finance-manager'`, () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        const app = fixture.componentInstance
-        expect(app.title).toEqual('finance-manager')
-    })
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent)
-        fixture.detectChanges()
-        const compiled = fixture.nativeElement as HTMLElement
-        expect(compiled.querySelector('.content span')?.textContent).toContain(
-            'finance-manager app is running!'
-        )
-    })
-})
+    it(`should have as title 'Finance Manager'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.title).toEqual('Finance Manager');
+    });
+});
--- src/app/components/navigation/navigation.component.spec.ts
+++ src/app/components/navigation/navigation.component.spec.ts
@@ -1,21 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationComponent } from './navigation.component';
+import { UserService } from '../../services/user.service';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('NavigationComponent', () => {
-  let component: NavigationComponent;
-  let fixture: ComponentFixture<NavigationComponent>;
+    let component: NavigationComponent;
+    let fixture: ComponentFixture<NavigationComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [NavigationComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [NavigationComponent],
+            providers: [
+                {
+                    provide: MatDialog,
+                    useValue: jasmine.createSpyObj('MatDialog', ['closeAll']),
+                },
+                {
+                    provide: UserService,
+                    useValue: jasmine.createSpyObj('UserService', [
+                        'setCurrentUser',
+                        'getCurrentUserObservable',
+                    ]),
+                },
+            ],
+        });
+        fixture = TestBed.createComponent(NavigationComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(NavigationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
