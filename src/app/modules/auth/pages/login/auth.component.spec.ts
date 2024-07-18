f0b49c7ec49dbf05e527ef48e1f30a918c28c810
olatu <olya.latun@taxsystems.com>
2024-07-18 13:36:26 +0300

    chore: auth component tests

--- src/app/modules/auth/pages/login/auth.component.spec.ts
+++ src/app/modules/auth/pages/login/auth.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { AuthComponent } from './auth.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from '../../../../services/user.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AuthComponent } from './login.component';
-
-describe('LoginComponent', () => {
+describe('AuthComponent', () => {
     let component: AuthComponent;
     let fixture: ComponentFixture<AuthComponent>;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [RouterTestingModule, ReactiveFormsModule],
             declarations: [AuthComponent],
+            providers: [
+                HttpClient,
+                HttpHandler,
+                {
+                    provide: UserService,
+                    useValue: jasmine.createSpyObj('UserService', [
+                        'setCurrentUser',
+                    ]),
+                },
+                {
+                    provide: MatDialogRef,
+                    useValue: jasmine.createSpyObj('MatDialogRef', ['close']),
+                },
+                {
+                    provide: MatDialog,
+                    useValue: jasmine.createSpyObj('MatDialog', ['closeAll']),
+                },
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
         });
         fixture = TestBed.createComponent(AuthComponent);
         component = fixture.componentInstance;
