6d324a7472438fa63751ccfb7717d30dd604a73d
olatu <olya.latun@taxsystems.com>
2024-07-18 16:36:49 +0300

    chore: add settings dialog tests

--- src/app/components/settings-dialog/settings-dialog.component.spec.ts
+++ src/app/components/settings-dialog/settings-dialog.component.spec.ts
@@ -9,12 +9,21 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogRef } from '@angular/material/dialog';
+import { WalletService } from '../../services/wallet/wallet.service';
+import { LocalStorageKeys } from '../../enums/local-storage-keys.enum';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { UserSettings } from '../../models/user-settings.model';
 
-describe('SettingsDialogComponent', () => {
+fdescribe('SettingsDialogComponent', () => {
     let component: SettingsDialogComponent;
     let fixture: ComponentFixture<SettingsDialogComponent>;
+    let dialogRefSpy: jasmine.SpyObj<MatDialogRef<SettingsDialogComponent>>;
+    let localStorageService: LocalStorageService;
 
-    beforeEach(() => {
+    beforeEach(async () => {
+        dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+
         TestBed.configureTestingModule({
             imports: [
                 ReactiveFormsModule,
@@ -26,19 +35,59 @@ describe('SettingsDialogComponent', () => {
             ],
             declarations: [SettingsDialogComponent],
             providers: [
+                LocalStorageService,
                 {
                     provide: DialogRef,
-                    useValue: jasmine.createSpyObj('DialogRef', ['close']),
+                    useValue: dialogRefSpy,
                 },
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         });
+
         fixture = TestBed.createComponent(SettingsDialogComponent);
+        localStorageService = TestBed.inject(LocalStorageService);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should set settings', () => {
+        expect(component.settings).toBeDefined();
+    });
+
+    it('should reset default settings on reset click', () => {
+        component.onResetClick();
+
+        expect(component.settingsForm.controls.income.value).toBe(0);
+        expect(component.settingsForm.controls.currency.value).toBe('');
+        expect(component.settingsForm.controls.creditCard.value).toBe(false);
+    });
+
+    it('should save settings to local storage', () => {
+        const localStorageGetSpy = spyOn(localStorageService, 'setItem');
+        spyOn(localStorageService, 'getItem').and.returnValue('123');
+
+        component.settingsForm.controls.income.setValue('123');
+        component.settingsForm.controls.currency.setValue('USD');
+        component.settingsForm.controls.creditCard.setValue(false);
+
+        const settings: UserSettings = new UserSettings({
+            currency: 'USD',
+            income: 123,
+            creditCard: false,
+        });
+
+        component.onSaveClick();
+
+        expect(localStorageGetSpy).toHaveBeenCalledWith(
+            `${LocalStorageKeys.Settings}-123`,
+            settings,
+        );
+    });
+
+    it('should close dialog on close', () => {
+        const event = new Event('click');
+        component.closeDialog(event);
+
+        expect(dialogRefSpy.close).toHaveBeenCalled();
     });
 });
