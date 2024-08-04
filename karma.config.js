5589e6faa9b1ab44fb0556cfd97d4a71f521df0a
olatu <olya.latun@taxsystems.com>
2024-08-04 12:56:51 +0300

    chore: update karma config to run a ci pipeline

--- karma.config.js
+++ karma.config.js
@@ -12,6 +12,13 @@ module.exports = function (config) {
         client: {
             clearContext: false,
         },
+        browsers: ['ChromeHeadlessNoSandbox'],
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox'],
+            },
+        },
         coverageReporter: {
             dir: require('path').join(__dirname, './coverage/my-app-name'),
             subdir: '.',
@@ -22,8 +29,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
-        singleRun: false,
+        singleRun: true,
         restartOnFileChange: true,
     });
 };
